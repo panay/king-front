f2935a96a8e067bfb6a431a5e9f7ef110faf6094
panay <panay@taptaxi.su>
2021-02-09 00:05:45 +0400

    feat(core): tailwind цвета

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Button from "../ui/Button";
 import {ReactComponent as Logo} from "../infrastructure/assets/images/svgs/logo.svg";
+import {ReactComponent as PicCampaign} from "../infrastructure/assets/images/svgs/pic-campaign.svg";
+import {ReactComponent as IcHome} from "../infrastructure/assets/images/svgs/ic-home.svg"
 
 function App() {
   return (
     <div className="max-w-sm m-auto">
-        <Logo className="text-blue-500"/>
+        <Logo className="text-button-link"/>
+        <PicCampaign />
+        <IcHome className="text-success" />
       <Button value="Применить" />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#fff",
+        transparent: "transparent",
+        "button-link": "#0052cc",
+        text: "#181818",
+        "icon-grey": "#8f92a1",
+        "input-grey": "#f4f4f6",
+        "dusty-orange": "#ff531d",
+        success: "#3eb969",
+        "chocolate-brown": "#3f2e00",
+        seagreen: "#2e8b58",
+        ice: "#e5fbef",
+        darkblue: "#5243aa",
+        orange: "#ff991f",
+        teal: "#00875a",
+        "acid-green": "#6acd00",
+        magenta: "#ec31ff",
+        moccasin: "#fdf0ba",
+        skyblue: "#41b2ff",
+        seashell: "#fff3f0",
+        orangered: "#f2461f",
+        "lighten-blue": "#f7faff",
+      },
+    },
   },
   variants: {
     extend: {},
